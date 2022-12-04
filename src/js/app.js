9c20a8f6deca06add233fbd7546bd625454fa0e4
mvatanasov18 <54852701+mvatanasov18@users.noreply.github.com>
2022-12-04 13:11:29 +0200

    Fix scaling

--- src/js/app.js
+++ src/js/app.js
@@ -2,9 +2,9 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
-  const nz = document.querySelector(".image");
-  mai.addEventListener("click", function() {
-    mai.style.transform = "scale(2)";
+  const element = document.querySelector(".image");
+  element.addEventListener("click", function() {
+    element.style.transform = "scale(2)";
   });
   const button = document.querySelector(".button");
   button.addEventListener("click", () => {
