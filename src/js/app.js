559f49ccd299c7ebc8ede617f1c7a5ac6d52e284
mvatanasov18 <54852701+mvatanasov18@users.noreply.github.com>
2022-12-04 13:06:06 +0200

    Transform image

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,10 @@ import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
-
+  const nz = document.querySelector(".image");
+  mai.addEventListener("click", function() {
+    mai.style.transform = "scale(2)";
+  });
   const button = document.querySelector(".button");
   button.addEventListener("click", () => {
     alert("💣");
